1890871d9ebf8526107d5a61edfd52605ca6f502
Vaja <vajadipak968@gmail.com>
2024-01-11 19:49:26 +0530

    11/01/24

---
+++ Book_Barter-Design/Views/header.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Chronicle Education Category Bootstrap Responsive website Template | Home :: w3layouts</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Chronicle Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+    SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- Custom Theme files -->
+    <link href="<?php echo $this->assets_url; ?>/css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+    <!-- shop css -->
+    <link href="<?php echo $this->assets_url; ?>/css/shop.css" type="text/css" rel="stylesheet" media="all">
+    <!-- common-css -->
+    <link href="<?php echo $this->assets_url; ?>/css/style.css" type="text/css" rel="stylesheet" media="all">
+    <!-- gallery desoslide -->
+    <link rel="stylesheet" href="<?php echo $this->assets_url; ?>/css/animate.min.css">
+    <link rel="stylesheet" href="<?php echo $this->assets_url; ?>/css/jquery.desoslide.css">
+    <!-- font-awesome icons -->
+    <link href="<?php echo $this->assets_url; ?>/css/font-awesome.css" rel="stylesheet">
+    <!-- footer stylesheet -->
+    <link href="<?php echo $this->assets_url; ?>/css/footer3.css" type="text/css" rel="stylesheet" media="all">
+    <!-- Range-slider-css -->
+    <link rel="stylesheet" type="text/css" href="<?php echo $this->assets_url; ?>/css/jquery-ui1.css">
+    <!-- checkout css -->
+    <link href="<?php echo $this->assets_url; ?>/css/checkout.css" type="text/css" rel="stylesheet" media="all">
+    <!-- easy-responsive-tabs css -->
+    <link href="<?php echo $this->assets_url; ?>/css/easy-responsive-tabs.css" type="text/css" media="all"
+        rel="stylesheet" />
+    <!-- online-fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- logo -->
+    <link href="//fonts.googleapis.com/css?family=Fredericka+the+Great" rel="stylesheet">
+    <!-- titles -->
+    <link href="//fonts.googleapis.com/css?family=Merriweather+Sans:300,300i,400,400i,700,700i,800,800i"
+        rel="stylesheet">
+    <!-- body -->
+    <link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
+        rel="stylesheet">
+    <!-- //online-fonts -->
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <div id="home">
+        <!-- header -->
+        <!-- navbar -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="main-nav">
+                <div class="container">
+
+                    <div class="navbar-header page-scroll">
+                        <h1>
+                            <a class="navbar-brand" href="home">Chronicle</a>
+                        </h1>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse navbar-ex1-collapse nav-right">
+                        <ul class="nav navbar-nav navbar-left cl-effect-15">
+                            <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                            <li class="hidden">
+                                <a class="page-scroll" href="#page-top"></a>
+                            </li>
+                            <li>
+                                <a href="home">Home</a>
+                            </li>
+                            <li>
+                                <a href="about">about us</a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle effect-3" data-toggle="dropdown">shop
+                                    <b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="product">product catalog</a>
+                                    </li>
+                                    <li>
+                                        <a href="checkout">checkout</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="contact">Contact us</a>
+                            </li>
+                            <li>
+                                <a href="login" title="SignIn & SignUp">
+                                    <span class="fa fa-user nav-icon" aria-hidden="true"></span>
+                                </a>
+                            </li>
+
+                        </ul>
+                        <!-- search-bar -->
+                        <div class="search-bar-agileits">
+                            <div class="cd-main-header">
+                                <ul class="cd-header-buttons">
+                                    <li>
+                                        <a class="cd-search-trigger" href="#cd-search">
+                                            <span></span>
+                                        </a>
+                                    </li>
+                                </ul>
+                                <!-- cd-header-buttons -->
+                            </div>
+                            <div id="cd-search" class="cd-search">
+                                <form action="#" method="post">
+                                    <input name="Search" type="search" placeholder="Type and Hit Enter...">
+                                </form>
+                            </div>
+                        </div>
+                        <!-- //search-bar ends here -->
+                        <!-- shopping cart -->
+                        <div class="cart-mainf">
+                            <div class="chrcart chrcart2 cart cart box_1">
+                                <a href="checkout">
+                                    <button class="top_chr_cart" type="submit" name="submit" value="">
+                                        <i class="fa fa-cart-arrow-down" aria-hidden="true"></i>
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                        <!-- //shopping cart ends here -->
+                    </div>
+                    <!-- /.navbar-collapse -->
+                    <div class="clearfix"></div>
+                </div>
+                <!-- /.container -->
+            </div>
+        </nav>
+        <!-- //navbar ends here -->
